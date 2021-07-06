b38da8278a63d8699fc1e29665847bccdd21dae8
Martin Albert <martin.albert.187@gmail.com>
2021-07-06 14:13:57 +0200

    chore: format & typos

--- server.js
+++ server.js
@@ -1,214 +1,217 @@
-const childProcess = require("child_process");
-const path = require("path");
-const dotenv = require("dotenv");
-const { promisify } = require("util");
-const exec = promisify(childProcess.exec);
+const childProcess = require('child_process')
+const path = require('path')
+const dotenv = require('dotenv')
+const { promisify } = require('util')
 
-const { sendEmail, fetchUserRepos, getAddress } = require("./superface");
+const { sendEmail, fetchUserRepos, getAddress } = require('./superface')
+
+const exec = promisify(childProcess.exec)
 
 async function main() {
-  // load enviroment variables
-  dotenv.config();
+    // load enviroment variables
+    dotenv.config()
 
-  // Require the fastify framework and instantiate it
-  const server = require("fastify")({
-    // set this to true for detailed logging:
-    logger: false
-  });
+    // Require the fastify framework and instantiate it
+    const server = require('fastify')({
+        // set this to true for detailed logging:
+        logger: false,
+    })
 
-  // Setup our static files
-  await server.register(require("fastify-static"), {
-    root: path.join(__dirname, "public"),
-    prefix: "/" // optional: default '/'
-  });
+    // Setup our static files
+    await server.register(require('fastify-static'), {
+        root: path.join(__dirname, 'public'),
+        prefix: '/', // optional: default '/'
+    })
 
-  // fastify-formbody lets us parse incoming forms
-  await server.register(require("fastify-formbody"));
+    // fastify-formbody lets us parse incoming forms
+    await server.register(require('fastify-formbody'))
 
-  // point-of-view is a templating manager for fastify
-  await server.register(require("point-of-view"), {
-    engine: {
-      handlebars: require("handlebars")
-    }
-  });
+    // point-of-view is a templating manager for fastify
+    await server.register(require('point-of-view'), {
+        engine: {
+            handlebars: require('handlebars'),
+        },
+    })
 
-  // load and parse SEO data
-  const seo = require("./src/seo.json");
-  if (seo.url === "glitch-default") {
-    seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-  }
-
-  // Our home page route, this pulls from src/pages/index.hbs
-  server.get("/", function(request, reply) {
-    let params = { seo: seo };
-    reply.view("/src/pages/index.hbs", params);
-  });
-
-  // A POST route to handle and react to form submissions
-  server.post("/", async function(request, reply) {
-    const { email, type } = request.body;
-
-    // "Validate form input"
-    let errorMessage;
-    if (!email || email === "") {
-      errorMessage = "please enter your email";
-    } else if (!type) {
-      errorMessage = "please choose one type of usecase";
+    // load and parse SEO data
+    const seo = require('./src/seo.json')
+    if (seo.url === 'glitch-default') {
+        seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`
     }
 
-    switch (type) {
-      case "classic":
-        if (!request.body.subject) {
-          errorMessage = "please enter some subject";
-        } else if (!request.body.text) {
-          errorMessage = "please your message";
+    // Our home page route, this pulls from src/pages/index.hbs
+    server.get('/', function (request, reply) {
+        let params = { seo: seo }
+        reply.view('/src/pages/index.hbs', params)
+    })
+
+    // A POST route to handle and react to form submissions
+    server.post('/', async function (request, reply) {
+        const { email, type } = request.body
+
+        // "Validate form input"
+        let errorMessage
+        if (!email || email === '') {
+            errorMessage = 'please enter your email'
+        } else if (!type) {
+            errorMessage = 'please choose one type of usecase'
         }
-        break;
 
-      case "user-repos":
-        if (!request.body.user) {
-          errorMessage = "please enter username";
-        } else if (!request.body.service) {
-          errorMessage = "please select a provider";
+        switch (type) {
+            case 'classic':
+                if (!request.body.subject) {
+                    errorMessage = 'please enter some subject'
+                } else if (!request.body.text) {
+                    errorMessage = 'please enter your message'
+                }
+                break
+
+            case 'user-repos':
+                if (!request.body.user) {
+                    errorMessage = 'please enter username'
+                } else if (!request.body.service) {
+                    errorMessage = 'please select a provider'
+                }
+                break
+
+            case 'address':
+                if (!request.body.latitude || !request.body.longitude) {
+                    errorMessage = 'please enter latitude and longitude'
+                } else if (!request.body.service) {
+                    errorMessage = 'please select a provider'
+                }
         }
-        break;
 
-      case "address":
-        if (!request.body.latitude || !request.body.longitude) {
-          errorMessage = "please enter latitude and longitude";
-        } else if (!request.body.service) {
-          errorMessage = "please select a provider";
+        // Do the business
+        let failoverInfo, success, message, to, subject, mailBody, log
+        if (!errorMessage) {
+            to = request.body.email
+
+            switch (type) {
+                case 'classic':
+                    // Inject the email subject & body from request
+                    subject = request.body.subject
+                    mailBody = request.body.text
+                    break
+
+                case 'user-repos':
+                    // Use superface to fetch the user repositories
+                    const { repos } = await fetchUserRepos(
+                        request.body.user,
+                        request.body.service
+                    )
+
+                    // Format the email subject & body
+                    subject = 'List of VCS repositories'
+                    mailBody = `Hi,\n\nhere is the list of repositories for ${request.body.user}:\n\n`
+
+                    // Format recieved repositories
+                    if (repos && repos.length) {
+                        repos.forEach((entry) => {
+                            mailBody += entry.name + '\n'
+                        })
+                    } else {
+                        mailBody += 'no repositories found'
+                    }
+
+                    mailBody += '\n\n– Yours, https//superface.ai'
+                    break
+
+                case 'address':
+                    const { latitude, longitude } = request.body
+
+                    // Use Superface to get information about given location
+                    const addressResponse = await getAddress(
+                        latitude,
+                        longitude,
+                        request.body.service
+                    )
+
+                    // Format the email subject & body
+                    subject = 'Formatted address'
+                    mailBody = `Hi,\n\naddress for given latitude: ${latitude} and longitude: ${longitude} is:\n\n${addressResponse.message}\n\n- Yours, https//superface.ai`
+                    break
+            }
+
+            // Use superface to send out the email
+            ;({ failoverInfo, success, message, log } = await sendEmail(
+                to,
+                subject,
+                mailBody
+            ))
         }
-    }
 
-    // Do the business
-    let failoverInfo, success, message, to, subject, mailBody, log;
-    if (!errorMessage) {
-      to = request.body.email;
+        // Fills parametres for the view
+        let params = {
+            seo,
+            message,
+            errorMessage,
+            success,
+            resultLog: JSON.stringify(log, null, 2),
+            failoverInfo,
+        }
 
-      switch (type) {
-        case "classic":
-          subject = request.body.subject;
-          mailBody = request.body.text;
-          break;
+        reply.view('/src/pages/index.hbs', params)
+    })
 
-        case "user-repos":
-          // Use superface to fetch the user repositories
-          const reposResponse = await fetchUserRepos(
-            request.body.user,
-            request.body.service
-          );
+    // webhook route for synchronization with github repository
+    // https://github.com/superfaceai/email-example
+    server.post('/git', async function (request, reply) {
+        if (process.env.SECRET === undefined) {
+            await reply.status(500)
+        }
 
-          subject = "List of VCS repositories";
+        const commitPushed = request.headers['x-github-event'] === 'push'
+        const refArray = commitPushed ? request.body.ref.split('/') : []
+        const branch = refArray[refArray.length - 1]
 
-          // Format the email body
-          mailBody = `Hi,\n\nhere is the list of repositories for ${request.body.user}:\n\n`;
+        const pushedToMaster = commitPushed && branch === 'main'
+        const pullRequestMerged =
+            request.headers['x-github-event'] === 'pullrequest' &&
+            request.body.action === 'closed' &&
+            request.body.pull_request.merged
 
-          if (
-            reposResponse &&
-            reposResponse.repos &&
-            reposResponse.repos.length
-          ) {
-            reposResponse.repos.forEach(entry => {
-              mailBody += entry.name + "\n";
-            });
-          } else {
-            mailBody += "no repositories found";
-          }
+        const hmac = crypto.createHmac('sha1', process.env.SECRET)
+        const sig =
+            'sha1=' + hmac.update(JSON.stringify(request.body)).digest('hex')
 
-          mailBody += "\n\n– Yours, https//superface.ai";
-          break;
+        if (
+            (pushedToMaster || pullRequestMerged) &&
+            sig === request.headers['x-hub-signature']
+        ) {
+            {
+                const { stdout, stderr } = await exec('chmod 777 git.sh')
+                server.log.info(stdout)
+                server.log.error(stderr)
+            }
+            {
+                const { stdout, stderr } = await exec('./git.sh')
+                server.log.info(stdout)
+                server.log.error(stderr)
+            }
+            {
+                const { stdout, stderr } = await exec('refresh')
+                server.log.info(stdout)
+                server.log.error(stderr)
+            }
 
-        case "address":
-          const { latitude, longitude } = request.body;
-          if (!latitude || !longitude) {
-            errorMessage = "please enter latitude and longitude";
-          } else if (!request.body.service) {
-            errorMessage = "please select a provider";
-          }
+            server.log.info('> [GIT] Updated with origin/main')
+        }
 
-          const addressResponse = await getAddress(
-            latitude,
-            longitude,
-            request.body.service
-          );
+        reply.status(200)
+        reply.send('webhook completed')
+    })
 
-          subject = "Formatted address";
-          mailBody = `Hi,\n\naddress for given latitude: ${latitude} and longitude: ${longitude} is:\n\n${addressResponse.message}\n\n- Yours, https//superface.ai`;
-          break;
-      }
+    // Run the server and report out to the logs
+    server.listen(process.env.PORT ?? 64187, (err, address) => {
+        if (err) {
+            server.log.error(err)
+            process.exit(1)
+        }
 
-      // Use superface to send out the email
-      const mailResponse = await sendEmail(to, subject, mailBody);
-
-      // Response
-      ({ failoverInfo, success, message, log } = mailResponse);
-    }
-
-    // Fills parametres sent to Handlebar views
-    let params = { seo, message, errorMessage, success, log, failoverInfo };
-
-    reply.view("/src/pages/index.hbs", params);
-  });
-
-  // webhook route for synchronization with github repository
-  // https://github.com/superfaceai/email-example
-  server.post("/git", async function(request, reply) {
-    if (process.env.SECRET === undefined) {
-      await reply.status(500);
-    }
-
-    const commitPushed = request.headers["x-github-event"] === "push";
-    const refArray = commitPushed ? request.body.ref.split("/") : [];
-    const branch = refArray[refArray.length - 1];
-
-    const pushedToMaster = commitPushed && branch === "main";
-    const pullRequestMerged =
-      request.headers["x-github-event"] === "pullrequest" &&
-      request.body.action === "closed" &&
-      request.body.pull_request.merged;
-
-    const hmac = crypto.createHmac("sha1", process.env.SECRET);
-    const sig =
-      "sha1=" + hmac.update(JSON.stringify(request.body)).digest("hex");
-
-    if (
-      (pushedToMaster || pullRequestMerged) &&
-      sig === request.headers["x-hub-signature"]
-    ) {
-      {
-        const { stdout, stderr } = await exec("chmod 777 git.sh");
-        server.log.info(stdout);
-        server.log.error(stderr);
-      }
-      {
-        const { stdout, stderr } = await exec("./git.sh");
-        server.log.info(stdout);
-        server.log.error(stderr);
-      }
-      {
-        const { stdout, stderr } = await exec("refresh");
-        server.log.info(stdout);
-        server.log.error(stderr);
-      }
-
-      server.log.info("> [GIT] Updated with origin/master");
-    }
-
-    reply.status(200);
-    reply.send("webhook completed");
-  });
-
-  // Run the server and report out to the logs
-  server.listen(process.env.PORT, (err, address) => {
-    if (err) {
-      server.log.error(err);
-      process.exit(1);
-    }
-
-    console.log(`Your app is listening on ${address}`);
-    server.log.info(`server listening on ${address}`);
-  });
+        console.log(`Your app is listening on ${address}`)
+        server.log.info(`server listening on ${address}`)
+    })
 }
 
-main();
+main()
